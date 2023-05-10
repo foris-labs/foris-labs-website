c384bb934e55d9d119df1cbefec82ca36560098f
Dfuncoder <judembakwe1@gmail.com>
2023-05-10 01:14:37 +0100

    first commit

---
+++ app/Http/Requests/SocialLoginRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class SocialLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // Allow any user to make a social login request
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'provider' => 'required|in:facebook,google', // Only allow Facebook or Google as providers
+            'access_token' => 'required|string', // The access token provided by the social network
+        ];
+    }
+}
