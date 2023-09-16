950e38f91ac21f2b879391dcf7d935e2271fa3b1
Kyrian Obikwelu <koshnawaza@gmail.com>
2023-09-16 15:37:58 +0100

    Update social media sidebar

--- resources/views/layouts/guest/footer.blade.php
+++ resources/views/layouts/guest/footer.blade.php
@@ -23,10 +23,10 @@
                     <h2 class="font-heading mb-6 text-sm font-bold text-blue-950 uppercase">Follow us</h2>
                     <ul class="text-gray-500 font-medium">
                         <li class="mb-2">
-                            <a href="https://github.com/themesberg/flowbite" class="hover:underline ">Facebook</a>
+                            <a href="https://web.facebook.com/profile.php?id=100067094550865" class="hover:underline ">Facebook</a>
                         </li>
                         <li>
-                            <a href="https://discord.gg/4eeurUVvTy" class="hover:underline">Twitter</a>
+                            <a href="https://twitter.com/forislabs?lang=en" class="hover:underline">Twitter</a>
                         </li>
                     </ul>
                 </div>
@@ -69,13 +69,16 @@
                 </a>
                 <a href="https://linkedin.com/company/foris-labs/" class="text-gray-500 hover:text-gray-900">
                     <svg viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4">
-                        <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
+                        <path
+                            d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
                     </svg>
                     <span class="sr-only">LinkedIn page</span>
                 </a>
                 <a href="mailto:forislabs@gmail.com" class="text-gray-500 hover:text-gray-900">
-                    <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"  aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"></path>
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"
+                         aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"></path>
                     </svg>
                     <span class="sr-only">Email account</span>
                 </a>
@@ -88,49 +91,46 @@
     <div
         class="fixed inset-y-0 left-0 flex h-screen w-[50px] flex-col items-center justify-center transition-transform duration-500"
         :class="{ 'translate-x-[-50px]': hidden, 'translate-x-0': !hidden }">
-        <a
-            class="bg-ash-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
+        <a href="https://web.facebook.com/profile.php?id=100067094550865" target="_blank"
+            class="bg-gray-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
             <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                 class="main-grid-item-icon" fill="none">
+                 fill="none">
                 <path
                     d="m17.543 13.398.661-4.31h-4.136V6.29c0-1.18.578-2.329 2.43-2.329h1.88V.291S16.673 0 15.042 0c-3.407 0-5.633 2.064-5.633 5.802v3.285H5.622v4.311h3.786v10.42a15.015 15.015 0 0 0 4.66 0v-10.42h3.475Z"
                     fill="currentColor"/>
             </svg>
         </a>
-        <a
-            class="bg-ash-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
-            <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                 class="main-grid-item-icon" fill="none">
+        <a href="https://twitter.com/forislabs?lang=en" target="_blank"
+            class="bg-gray-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
+            <svg viewBox="0 0 24 24" aria-hidden="true" class="w-4 h-4" fill="currentColor">
                 <path
-                    d="M7.548 21.501c9.056 0 14.01-7.503 14.01-14.01 0-.213 0-.425-.015-.636A10.02 10.02 0 0 0 24 4.305a9.815 9.815 0 0 1-2.828.775 4.94 4.94 0 0 0 2.165-2.723 9.865 9.865 0 0 1-3.127 1.195 4.929 4.929 0 0 0-8.391 4.491A13.98 13.98 0 0 1 1.67 2.898a4.928 4.928 0 0 0 1.525 6.573A4.88 4.88 0 0 1 .96 8.855v.063a4.926 4.926 0 0 0 3.95 4.826 4.914 4.914 0 0 1-2.223.085 4.93 4.93 0 0 0 4.6 3.42A9.88 9.88 0 0 1 0 19.288a13.941 13.941 0 0 0 7.548 2.208"
-                    fill="currentColor"/>
+                    d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
             </svg>
-            </i></a>
-        <a
-            class="bg-ash-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
+        </a>
+        <a href="https://linkedin.com/company/foris-labs/" target="_blank"
+            class="bg-gray-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
             <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                 class="main-grid-item-icon" fill="none">
+                 fill="none">
                 <path
                     d="M5.372 24H.396V7.976h4.976V24ZM2.882 5.79C1.29 5.79 0 4.474 0 2.883a2.882 2.882 0 1 1 5.763 0c0 1.59-1.29 2.909-2.881 2.909ZM23.995 24H19.03v-7.8c0-1.86-.038-4.243-2.587-4.243-2.587 0-2.984 2.02-2.984 4.109V24H8.49V7.976h4.772v2.186h.07c.664-1.259 2.287-2.587 4.708-2.587 5.035 0 5.961 3.316 5.961 7.623V24h-.005Z"
                     fill="currentColor"/>
             </svg>
         </a>
-        <a
-            class="bg-ash-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
-            <!-- https://feathericons.dev/?search=mail&iconset=feather -->
+        <a href="mailto:forislabs@gmail.com"
+            class="bg-gray-800 my-1 flex h-8 w-8 items-center justify-center rounded-full text-white transition duration-500 hover:rotate-[360deg] hover:transform hover:bg-orange-500 hover:text-white">
             <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
-                 class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round"
+                 fill="none" stroke="currentColor" stroke-linecap="round"
                  stroke-linejoin="round" stroke-width="2">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/>
                 <polyline points="22,6 12,13 2,6"/>
             </svg>
-            </i></a>
+        </a>
     </div>
     <div
         class="fixed inset-y-0 left-0 z-10 flex h-screen w-[50px] flex-col items-start justify-center transition-transform duration-500"
         :class="{ 'translate-x-[-50px]': !hidden, 'translate-x-0': hidden }">
         <button @click="hidden = !hidden"
-                class="bg-ash-800/50 my-1 flex h-8 w-6 items-center justify-start rounded-r-full text-white transition duration-500 hover:bg-orange-500 hover:text-white">
+                class="bg-gray-800/50 my-1 flex h-8 w-6 items-center justify-start rounded-r-full text-white transition duration-500 hover:bg-orange-500 hover:text-white">
             <svg class="h-4 w-4" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
                  xmlns="http://www.w3.org/2000/svg">
                 <path
